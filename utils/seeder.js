09c56fdfc0929870a6e05e844bd9e8c54ec40719
hareeshbhuma <hareeshbhuma@gmail.com>
2019-11-06 16:21:25 -0600

    Seeder updated for student

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,5 +1,6 @@
 const Datastore = require('nedb') // set up a temporary (in memory) database
 const instructorData = require('../data/instructors.json') // read in data file
+const studentData = require('../data/student.json') // read in data file
 
 // inject Express app to configure it - EVERYTHING in through argument list
 
@@ -13,9 +14,18 @@ module.exports = (app) => {
   // insert the sample data into our datastore
   db.instructors.insert(instructorData)
 
+  db.student = new Datastore() // new object property
+  db.student.loadDatabase() // call the loadDatabase method
+
+  // insert the sample data into our datastore
+  db.student.insert(studentData)
+
   // initialize app.locals (these objects are available to the controllers)
   app.locals.instructors = db.instructors.find(instructorData)
   console.log(`${app.locals.instructors.query.length} instructors seeded`)
+
+  app.locals.student = db.student.find(studentData)
+  console.log(`${app.locals.student.query.length} student seeded`)
 
   console.log('END Data Seeder. Sample data read and verified.')
 }
