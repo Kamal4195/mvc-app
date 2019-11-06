033f02cdf84309c9bf5fb5a3a13f036715bc2bc8
RaghunandanKumar <naishadamraghu@gmail.com>
2019-11-06 16:25:36 -0600

    added section seeder

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,7 +1,7 @@
 const Datastore = require('nedb') // set up a temporary (in memory) database
 const instructorData = require('../data/instructors.json') // read in data file
 const studentData = require('../data/student.json') // read in data file
-
+const sectionData = require('../data/section.json') // read in data file
 // inject Express app to configure it - EVERYTHING in through argument list
 
 module.exports = (app) => {
@@ -20,12 +20,23 @@ module.exports = (app) => {
   // insert the sample data into our datastore
   db.student.insert(studentData)
 
+  db.section = new Datastore() // new object property
+  db.section.loadDatabase() // call the loadDatabase method
+
+  // insert the sample data into our datastore
+  db.section.insert(sectionData)
+
+  
+
   // initialize app.locals (these objects are available to the controllers)
   app.locals.instructors = db.instructors.find(instructorData)
   console.log(`${app.locals.instructors.query.length} instructors seeded`)
 
   app.locals.student = db.student.find(studentData)
   console.log(`${app.locals.student.query.length} student seeded`)
+
+  app.locals.section = db.section.find(sectionData)
+  console.log(`${app.locals.section.query.length} section seeded`)
 
   console.log('END Data Seeder. Sample data read and verified.')
 }
